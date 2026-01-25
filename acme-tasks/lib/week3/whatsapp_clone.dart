da1f0fe48ddc8b6057c5b0507d69d394e825f5f0
Izen Abbas <izen.khokhar@gmail.com>
2026-01-25 23:38:37 +0500

    First Page Mostly Completed

--- acme-tasks/lib/week3/whatsapp_clone.dart
+++ acme-tasks/lib/week3/whatsapp_clone.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+List<String> contacts = List.filled(50, 'test');
+List<String> l1 = ['All', 'Unread', 'Favourites', 'Groups', ''];
+
 class WhatsappClone extends StatefulWidget {
   const WhatsappClone({super.key});
 
@@ -10,47 +13,113 @@ class WhatsappClone extends StatefulWidget {
 class _WhatsappCloneState extends State<WhatsappClone> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      automaticallyImplyLeading: false,
-      title: Text('Whatsapp', style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 174, 89)),
-    ),
-    actions: [Padding(
-      padding: const EdgeInsets.only(right: 8.0),
-      child: Icon(Icons.camera_alt_outlined),
-    ), Icon(Icons.more_vert)])
-    
-    ,body: Padding(
-      padding: const EdgeInsets.all(15.0),
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Whatsapp',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 0, 174, 89),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(Icons.camera_alt_outlined),
+          ),
+          Icon(Icons.more_vert),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
 
-      child: Column(children: [TextField(decoration: InputDecoration(prefixIcon: Icon(Icons.search),border: OutlineInputBorder(borderRadius: BorderRadius.circular(20), borderSide: BorderSide(color: Color.fromARGB(255, 89, 98, 105))) ,hintText: 'Ask Meta AI or Search'),),
-      Padding(
-        padding: const EdgeInsets.only(top: 15.0),
-        child: Row(children: [
-        Padding(
-          padding: const EdgeInsets.only(right: 8.0),
-          child: TextButton(onPressed: (){},style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(width: 1, color: Color.fromARGB(255, 8, 98, 105)))), child: Text('All'),),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right:8.0),
-          child: TextButton(onPressed: (){},style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),side: BorderSide(width: 1, color: Color.fromARGB(255, 8, 98, 105)))), child: Text('Unread'),),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right:8.0),
-          child: TextButton(onPressed: (){},style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(width: 1, color: Color.fromARGB(255, 8, 98, 105)))), child: Text('Favourites'),),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right:8.0),
-          child: TextButton(onPressed: (){},style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),side: BorderSide(width: 1, color: Color.fromARGB(255, 8, 98, 105)))), child: Text('Groups'),),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(right:8.0),
-          child: TextButton(onPressed: (){},style: TextButton.styleFrom(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100),side: BorderSide(width: 1, color: Color.fromARGB(255, 8, 98, 105)))), child: Icon(Icons.add),),
-        ),
-        ],),
-      )
-      
-      ],),
-    ));
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 89, 98, 105),
+                  ),
+                ),
+                hintText: 'Ask Meta AI or Search',
+              ),
+            ),
 
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0),
+              child: SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: l1.length,
+                  itemBuilder: (context, index) {
+                    if (l1[index] != '') {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 9.5),
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            side: BorderSide(width: 1),
+                          ),
+                          child: Text(
+                            l1[index],
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      );
+                    }
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 9.5),
+                      child: IconButton(
+                        onPressed: () {},
+                        style: IconButton.styleFrom(side: BorderSide(width: 1)),
+                        icon: Icon(Icons.add, color: Colors.black),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: contacts.length,
+                itemBuilder: (context, index) {
+                  if (index % 2 == 0) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        title: Text('Contact ' + (index + 1).toString()),
+                        tileColor: Colors.blue,
+                      ),
+                    );
+                  } else {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: ListTile(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        title: Text('Contact ' + (index + 1).toString()),
+                        tileColor: Colors.green,
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
