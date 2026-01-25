0e92e7effa7ede79fac5a6bc77ed0ca0a5e7c0ca
Izen Abbas <l230748@lhr.nu.edu.pk>
2026-01-26 00:53:09 +0500

    Refactor WhatsappClone UI for improved layout and styling

--- acme-tasks/lib/week3/whatsapp_clone.dart
+++ acme-tasks/lib/week3/whatsapp_clone.dart
@@ -14,7 +14,9 @@ class _WhatsappCloneState extends State<WhatsappClone> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         automaticallyImplyLeading: false,
         title: Text(
           'Whatsapp',
@@ -31,12 +33,11 @@ class _WhatsappCloneState extends State<WhatsappClone> {
           Icon(Icons.more_vert),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(15.0),
-
-        child: Column(
-          children: [
-            TextField(
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            child: TextField(
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
                 border: OutlineInputBorder(
@@ -48,77 +49,79 @@ class _WhatsappCloneState extends State<WhatsappClone> {
                 hintText: 'Ask Meta AI or Search',
               ),
             ),
-
-            Padding(
-              padding: const EdgeInsets.only(top: 15.0),
-              child: SizedBox(
-                height: 50,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: l1.length,
-                  itemBuilder: (context, index) {
-                    if (l1[index] != '') {
-                      return Padding(
-                        padding: const EdgeInsets.only(right: 9.5),
-                        child: TextButton(
-                          onPressed: () {},
-                          style: TextButton.styleFrom(
-                            side: BorderSide(width: 1),
-                          ),
-                          child: Text(
-                            l1[index],
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      );
-                    }
-                    return Padding(
-                      padding: const EdgeInsets.only(right: 9.5),
-                      child: IconButton(
-                        onPressed: () {},
-                        style: IconButton.styleFrom(side: BorderSide(width: 1)),
-                        icon: Icon(Icons.add, color: Colors.black),
-                      ),
-                    );
-                  },
-                ),
-              ),
+          ),
+      
+          Container(
+            width: double.infinity,
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.white, width: 8)
             ),
-
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: contacts.length,
-                itemBuilder: (context, index) {
-                  if (index % 2 == 0) {
-                    return Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        title: Text('Contact ' + (index + 1).toString()),
-                        tileColor: Colors.blue,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: l1.length,
+              itemBuilder: (context, index) {
+                if (l1[index] != '') {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 9.5),
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        side: BorderSide(width: 1),
                       ),
-                    );
-                  } else {
-                    return Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        title: Text('Contact ' + (index + 1).toString()),
-                        tileColor: Colors.green,
+                      child: Text(
+                        l1[index],
+                        style: TextStyle(color: Colors.black),
                       ),
-                    );
-                  }
-                },
-              ),
+                    ),
+                  );
+                }
+                return Padding(
+                  padding: const EdgeInsets.only(right: 9.5),
+                  child: IconButton(
+                    onPressed: () {},
+                    style: IconButton.styleFrom(side: BorderSide(width: 1)),
+                    icon: Icon(Icons.add, color: Colors.black),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+      
+          Expanded(
+            child: ListView.builder(
+              itemCount: contacts.length,
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              itemBuilder: (context, index) {
+                if (index % 2 == 0) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      title: Text('Contact ' + (index + 1).toString()),
+                      tileColor: Colors.blue,
+                    ),
+                  );
+                } else {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      title: Text('Contact ' + (index + 1).toString()),
+                      tileColor: Colors.green,
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
